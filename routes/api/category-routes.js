4b98f5e22b552b9be12a8f02318ec76fb3a386ba
Muramori71 <dannyroh2500@gmail.com>
2023-07-05 09:21:16 -0700

    deleted comment in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,24 +27,12 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  /*try {
+    try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
-  }*/
-  Category.create({
-    id: req.body.id,
-    category_name: req.body.category_name
-    
-  })
-    .then((newCategory) => {
-      // Send the newly created row as a JSON object
-      res.json(newCategory);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+  }
 });
 
 router.put('/:id', async (req, res) => {
